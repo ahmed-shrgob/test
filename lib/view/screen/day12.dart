176dd7076128807779576dd9151e148f07eb685c
ahmedshrgob <ahmedshrgob@gmail.com>
2024-01-26 23:07:24 +0300

    neew

--- lib/view/screen/day12.dart
+++ lib/view/screen/day12.dart
@@ -19,9 +19,10 @@ class _MyWidgetState extends State<MyWidget> {
           Image.network('https://e7.pngegg.com/pngimages/77/285/png-clipart-iphone-8-plus-iphone-7-plus-iphone-5-iphone-x-iphone-6s-iphone-apple-electronics-gadget.png'),
           Image.network("https://t-mobile.scene7.com/is/image/Tmusprod/fg-iPhone-i5-lineup1?ts=1698347184484&fmt=png-alpha&dpr=off"),
         ], options: CarouselOptions()),
-        Column(children: [
+      //   Column(children: [
+      // ])
+      //       ]),
       ])
-      ]),
     );
   }
 
