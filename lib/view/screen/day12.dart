1a9e120e79dc38c8b5e057b1854f64a2da4ce321
ahmedshrgob <ahmedshrgob@gmail.com>
2024-01-26 23:03:12 +0300

    new commit

--- lib/view/screen/day12.dart
+++ lib/view/screen/day12.dart
@@ -7,7 +7,7 @@ class MyWidget extends StatefulWidget {
   @override
   State<MyWidget> createState() => _MyWidgetState();
 }
-
+//new coment
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
@@ -17,8 +17,10 @@ class _MyWidgetState extends State<MyWidget> {
         CarouselSlider(items:[
           Image.network('https://e7.pngegg.com/pngimages/77/285/png-clipart-iphone-8-plus-iphone-7-plus-iphone-5-iphone-x-iphone-6s-iphone-apple-electronics-gadget.png'),
           Image.network('https://e7.pngegg.com/pngimages/77/285/png-clipart-iphone-8-plus-iphone-7-plus-iphone-5-iphone-x-iphone-6s-iphone-apple-electronics-gadget.png'),
-          Image.network('https://t-mobile.scene7.com/is/image/Tmusprod/fg-iPhone-i5-lineup1?ts=1698347184484&fmt=png-alpha&dpr=off'),
-        ], options: CarouselOptions())
+          Image.network("https://t-mobile.scene7.com/is/image/Tmusprod/fg-iPhone-i5-lineup1?ts=1698347184484&fmt=png-alpha&dpr=off"),
+        ], options: CarouselOptions()),
+        Column(children: [
+      ])
       ]),
     );
   }
